03b5167e5baecfa66377b5b545e29adb24c43b68
kdm97 <kdm97@DESKTOP-12STD32>
2021-05-12 02:11:16 +0900

    ProfileController 수정

--- src/main/java/com/shms/ProfileController.java
+++ src/main/java/com/shms/ProfileController.java
@@ -3,6 +3,7 @@ package com.shms;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
+	@Autowired
 	private Environment env;
 	
 	public ProfileController(Environment env) {
