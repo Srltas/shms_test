90ee25340b81aa67b783d805cf5fa9b206febe32
Srltas <kdm97912@gmail.com>
2021-05-11 22:35:12 +0900

    deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ cd $REPOSITORY
 
 ehco "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f shms-0.0.1-SNAPSHOT |grep jar)
+CURRENT_PID=$(pgrep -f shms-0.0.1-SNAPSHOT.jar)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
